2c3b975550252af2e3e0e4f2a025924087c8d082
Angel Medrano <asmedrano@usbos-sl500-05.zmag.local>
2011-08-16 17:03:27 -0400

    added zmox - fancybox version for ccoalesse

--- deploy/coalesse/010711/config.js
+++ deploy/coalesse/010711/config.js
@@ -15,7 +15,8 @@ var CORE_SOURCE_zmag = "http://ps.zmags.com/commerce/"
 
 var commerceProjectName_zmag = 'POC_Coalesee010711PO';
 var zmagsDebug = false;
-var zmagsAnalyticsEnabled = false;
+var zmagsGAnalyticsEnabled = false;
+//var zmagsAnalyticsEnabled = false;
 
 //full path to client's add to cart script (including parameters if using GET) 
 //EXAMPLE: addToCartURL = http://dev.kirnazabete.com/checkout/cart/add/uenc/aHR0cDovL2Rldi5raXJuYXphYmV0ZS5jb20vY2xvdGhlcy9jb2F0cy9zcGFydGEtY29hdA,,/product/12742/
@@ -47,24 +48,49 @@ var includePathZmags ='http://ps.zmags.com/poc/deploy/coalesse/010711/';
 	
 	Use includePathZmags+"asset/foo.css" to get CLIENT specific items
 */
-var jsIncludeFiles_zmag =    ["js/coalesse.js","js/cloud-zoom.1.0.2.js","js/jquery.dd.js"
+/*var jsIncludeFiles_zmag =    ["js/coalesse.js","js/cloud-zoom.1.0.2.js","js/jquery.dd.js"
           
             ];
 var cssIncludeFiles_zmag =   ["css/dpw.css","css/cloud-zoom.css","js/dd.css"
          
+            ];*/
+
+var jsIncludeFiles_zmag =    [  
+                              includePathZmags+"js/Zmox/jquery.zmox-1.js",
+                              includePathZmags+"js/coalesse.js",
+                              includePathZmags+"js/cloud-zoom.1.0.2.js",
+                              includePathZmags+"js/jquery.dd.js",
+                              
+          
             ];
+var cssIncludeFiles_zmag =  [includePathZmags+"css/dpw.css",
+                             includePathZmags+"css/cloud-zoom.css",
+                             includePathZmags+"js/dd.css",
+                             includePathZmags+"js/Zmox/jquery.zmox-1.css"
+         
+            ];
+
+
 
 /*
 ========================
 DO NOT CHANGE ANYTHING UNDER THIS LINE!
 ========================
 */
- 
+/* 
 //--- GET zmagsCommerce.js //
 var commercejs_zmag = document.createElement("script");
 
 // currently pointed to the dev environment maybe it should be a var?///
 commercejs_zmag.setAttribute("src", CORE_SOURCE_zmag +"corecommerce/resources/zmagsCommerce1.2.js");
+commercejs_zmag.setAttribute("type", "text/javascript");
+document.getElementsByTagName('head')[0].appendChild(commercejs_zmag);*/
+ 
+//--- GET zmagsCommerce.js //
+var commercejs_zmag = document.createElement("script");
+
+// currently pointed to the dev environment maybe it should be a var?///
+commercejs_zmag.setAttribute("src", CORE_SOURCE_zmag +"corecommerce/resources/zmagsCommerce1.1.4.js");
 commercejs_zmag.setAttribute("type", "text/javascript");
 document.getElementsByTagName('head')[0].appendChild(commercejs_zmag);
 
